ac073bec48f3b22c77d4a646deed500b12504075
anishkashyap2002 <anishkashyap311@gmail.com>
2024-04-10 03:09:08 +0530

    9th work

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const gateRight = document.getElementById("gate-right");
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
-    text.style.marginTop = value * 2.5 + "px";
-    gateLeft.style.left = value * 0.5 + "px";
-    gateLeft.style.marginTop = value * 1.5 + "px";
-    gateRight.style.left = value * -0.5 + "px";
-    gateRight.style.marginTop = value * 1.5 + "px";
+    text.style.marginTop = value * -0.5 + "px";
+    gateLeft.style.left = value * 0.35 + "px";
+    // gateLeft.style.marginTop = value * 1.5 + "px";
+    gateRight.style.left = value * -0.35 + "px";
+    // gateRight.style.marginTop = value * 1.5 + "px";
 });
