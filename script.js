121d52828ddb9d9ea3061f387bd7cc471878d094
Mitali011 <mitali112003@gmail.com>
2024-04-10 00:50:36 +0530

    image changed

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const text = document.getElementById("text");
-const treeLeft = document.getElementById("tree-left");
-const treeRight = document.getElementById("tree-right");
+const gateRight = document.getElementById("gate-right");
+const gateLeft = document.getElementById("gate-left");
+
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
     text.style.marginTop = value * 2.5 + "px";
-    treeLeft.style.left = value * -1.5 + "px";
-    treeRight.style.left = value * 1.5 + "px";
+    gateLeft.style.left = value * 0.35 + "px";
+    gateRight.style.left = value * -0.35 + "px";
 });
