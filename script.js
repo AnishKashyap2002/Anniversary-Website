ccb1595687687547f6ba5a80817faebd1adcd3a2
Mitali011 <mitali112003@gmail.com>
2024-04-12 04:21:20 +0530

    game completed

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const text = document.getElementById("text");
 const gateLeft = document.getElementById("gate-left");
 const gateRight = document.getElementById("gate-right");
+let target = document.getElementById("target");
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
@@ -10,3 +11,34 @@ window.addEventListener("scroll", () => {
     gateRight.style.left = value * -0.35 + "px";
     // gateRight.style.marginTop = value * 1.5 + "px";
 });
+
+game.addEventListener("click",()=> {
+    target.addEventListener('mouseover', win);
+
+    function win() {
+        target.style.transform = `scale(3)`;
+        setTimeout(s,200);
+
+        function s() {
+            target.style.transform = `scale(1)`;
+
+        }
+        
+        clearInterval(timer);
+        game.removeEventListener("click");
+    }
+
+    let timer = setInterval(jump, 400);
+    let counter = 0;
+
+    function jump() {
+        target.style.transform = `scale(1)`;
+
+        let x = Math.floor(Math.random() * (1000 - 0 + 1) - 0);
+        let y = Math.floor(Math.random() * (500 - 0 + 1) - 0);
+
+        counter++;
+        target.style.transform = `translate(${x}px, ${y}px)`;
+    }
+    
+})
